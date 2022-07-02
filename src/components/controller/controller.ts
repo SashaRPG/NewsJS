78abfd61a5ae8498a5bb8f0d2bab9b03a448c4b2
SashaRPG <uasasharpg@gmail.com>
2022-07-02 18:33:28 +0300

    feat: all files in controller folder are now TypeScript-compatible

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -1,7 +1,7 @@
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: Function): void {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,28 +10,30 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: Function): void | undefined {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
-        while (target !== newsContainer) {
-            if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
-                if (newsContainer.getAttribute('data-source') !== sourceId) {
-                    newsContainer.setAttribute('data-source', sourceId);
-                    super.getResp(
-                        {
-                            endpoint: 'everything',
-                            options: {
-                                sources: sourceId,
+        if (target && newsContainer) {
+            while (target !== newsContainer) {
+                if (target?.classList.contains('source__item')) {
+                    const sourceId = target.getAttribute('data-source-id');
+                    if (newsContainer.getAttribute('data-source') !== sourceId && sourceId) {
+                        newsContainer.setAttribute('data-source', sourceId);
+                        super.getResp(
+                            {
+                                endpoint: 'everything',
+                                options: {
+                                    sources: sourceId,
+                                },
                             },
-                        },
-                        callback
-                    );
+                            callback
+                        );
+                    }
+                    return;
                 }
-                return;
+                target = target.parentNode as HTMLElement;
             }
-            target = target.parentNode;
         }
     }
 }
--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,19 +1,28 @@
-class Loader {
-    constructor(baseLink, options) {
+import { ResponseObj, Options, ShowNews } from '../../interface';
+
+type API = {
+    apiKey: string;
+}
+
+abstract class Loader {
+    private baseLink: string;
+    private options: API;
+
+    constructor(baseLink: string, options: API) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
-        callback = () => {
+        respObj: ResponseObj,
+        callback: Function = () => {
             console.error('No callback for GET response');
         }
-    ) {
-        this.load('GET', endpoint, callback, options);
+    ): void {
+        this.load('GET', respObj.endpoint, callback, respObj.options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,23 +32,23 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    makeUrl(options: Options, endpoint: string): string {
+        const urlOptions: {[key: string]: string} = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
-        Object.keys(urlOptions).forEach((key) => {
+        Object.keys(urlOptions).forEach((key: string): void => {
             url += `${key}=${urlOptions[key]}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method: string, endpoint: string, callback: Function, options: Options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
-            .then((res) => res.json())
-            .then((data) => callback(data))
-            .catch((err) => console.error(err));
+            .then((res: Response) => res.json())
+            .then((data: ShowNews) => callback(data))
+            .catch((err: Error) => console.error(err));
     }
 }
 
