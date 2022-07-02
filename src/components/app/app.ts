cce8f0c093c933f7499690a54264e16a4b4c3139
SashaRPG <uasasharpg@gmail.com>
2022-07-02 18:15:35 +0300

    feat: app.ts was changed according to rules

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,17 +1,22 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
+import { ShowNews } from '../../interface';
+
 class App {
+    private controller = new AppController();
+    private view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
+    start(): void {
         document
             .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+            ?.addEventListener('click', (e: Event) => this.controller.getNews(e, (data: ShowNews) => this.view.drawNews(data)));
+        this.controller.getSources((data: ShowNews) => this.view.drawSources(data));
     }
 }
 
