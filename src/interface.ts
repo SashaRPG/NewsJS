016211f27cbfb24114aab4eaa9001232067b19c7
SashaRPG <uasasharpg@gmail.com>
2022-07-02 18:03:03 +0300

    feat: interfaces were created and files renamed from .js  to .ts

---
+++ src/interface.ts
@@ -0,0 +1,47 @@
+interface Info {
+    url: string;
+    name: string;
+    id: string;
+    language: string;
+    description: string;
+    country: string;
+    category: string;
+}
+
+interface Source {
+    readonly id: string;
+    name: string;
+}
+
+interface News {
+    url: string;
+    urlToImage: string;
+    title: string;
+    source: Source;
+    publishedAt: string;
+    description: string;
+    author: string;
+    content: string;
+}
+
+interface ShowSource {
+    status: string;
+    sources?: Array<Info>;
+}
+
+interface ShowNews {
+    status: string;
+    total?: number;
+    articles?: Array<News>;
+}
+
+interface Options {
+    sources?: string;
+}
+
+interface ResponseObj {
+    endpoint: string;
+    options?: Options;
+}
+
+export { Info, News, ShowNews, ShowSource, Options, ResponseObj };
