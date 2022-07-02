1d53f3cadcb9585cebd8a2d13e613fd9f1843e5f
SashaRPG <uasasharpg@gmail.com>
2022-07-02 19:06:33 +0300

    fix: changed webpack to work with TS

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
+    devtool: 'inline-source-map',
     mode: 'development',
     module: {
         rules: [
@@ -12,10 +13,15 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
         filename: 'index.js',
